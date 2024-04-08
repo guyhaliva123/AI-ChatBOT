852881557889d2f9847822228c7c7b1fd4a16a97
guyhaliva123 <116512226+guyhaliva123@users.noreply.github.com>
2024-04-08 13:48:29 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,9 +6,9 @@ from flask import Flask, render_template, request
 # 1. go to https://platform.openai.com/docs/overview
 # 2. on the side nav-bar press->API keys.
 # 3. copy the secret key if you don't have one yet, press the "Create new secret key" option.
-# 4. paste the key in the file called "my_file.txt" 
+# 4. paste the key in the file called "api_key.txt" 
 
-with open('my_file.txt', 'r') as file:
+with open('api_key.txt', 'r') as file:
     file_contents = file.read()
 
 # 5. if you skipped #4 than you can just replace the 'file_contents' with your secret key.
